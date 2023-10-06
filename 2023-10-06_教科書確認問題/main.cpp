5d6152d94b1dfd44d1c8155a6f4f612bff5fa078
angetagala <angetagala17@gmail.com>
2023-10-06 13:27:06 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023-10-06_教科書確認問題/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+/*足し算するsum関数を作成しなさい。
+ただし関数の実態はmain関数があるソースファイルとは別のファイルにしなさい。
+main関数で呼び出して使用すること。*/
+
+void Sum(int a, int b);
+
+int main()
+{
+	Sum(7, 8);
+}
+
---
+++ 2023-10-06_教科書確認問題/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*足し算するsum関数を作成しなさい。
+ただし関数の実態はmain関数があるソースファイルとは別のファイルにしなさい。
+main関数で呼び出して使用すること。*/
+
+void Sum(int a, int b)
+{
+	cout << "a + b = " << a + b << endl;
+}
